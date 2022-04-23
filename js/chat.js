9be8e03302c3fe23ceb8bed377bd5c54b63bd435
Pratyush Mittal <pratyushmittal@gmail.com>
2022-04-23 16:42:18 +0530

    Added proof-of-concept
    The finding and matching algorithm is working now.

--- js/chat.js
+++ js/chat.js
@@ -1,88 +1,131 @@
 /* global Alpine, Peer */
 
 (function() {
-	function setupAlpine () {
-		Alpine.store('me', {
-			name: '',
-			status: ''
-		})
-		Alpine.store('messages', [])
-		Alpine.store('them', false)
-		Alpine.store('room', {
-			id: null,
-			conn: null,
-		})
-		console.log('Alpine ready')
-	}
-
-	function sendMessage ($store, text) {
-		var message = {
-			from: $store.me,
-			text: text,
-			timestamp: Date.now(),
-		}
-		$store.room.conn.send(message)
-		$store.messages.push(message)
-	}
-
-	function receiveMessage ($store, message) {
-		$store.messages.push(message)
-	}
-
-	function hostRoom ($store) {
-		var roomID = 'host-yaari'
-		var peer = new Peer(roomID)
-
-		peer.on('open', function (id) {
-      console.log('registered as peer', id)
-			console.log('others can connect on it now', id)
-			$store.room.id = id
-		})
-		peer.on('error', function (err) {
-			if (err.type === 'unavailable-id') {
-				console.log('id unavailable, joining as a client')
-				return joinRoom($store, roomID)
-			}
-		})
-
-		peer.on('connection', function(conn) {
-			console.log('received connection', conn)
-			$store.room.conn = conn
-			// handle messages received from peer
-			conn.on('data', function(data) {
-				receiveMessage($store, data)
-			})
-		})
-	}
-
-	function joinRoom ($store, roomID) {
-		var peer = new Peer()
-		peer.on('open', function () {
-			var conn = peer.connect(roomID)
-
-			// on open will be launch when you successfully connect to PeerServer
-			conn.on('open', function() {
-				console.log('room opened')
-				$store.room.id = roomID
-				$store.room.conn = conn
-				sendMessage($store, 'joined')
-			})
-
-			// handle messages received from peer
-			conn.on('data', function(data) {
-				receiveMessage($store, data)
-			})
-		})
-	}
-
-	function setupEverything () {
-		console.log('setupEverything')
-		document.addEventListener('alpine:init', setupAlpine)
-		window.ChatActions = {
-			hostRoom: hostRoom,
-			joinRoom: joinRoom,
-			sendMessage: sendMessage,
-		}
-	}
-	setupEverything()
+  function setupAlpine () {
+    Alpine.store('me', {
+      name: '',
+      status: ''
+    })
+    Alpine.store('messages', [])
+    Alpine.store('them', false)
+    Alpine.store('conn', false)
+    Alpine.store('status', 'AlpineJS ready.')
+    console.log('Alpine ready')
+  }
+  
+  function sendSystemMessage (conn, data) {
+    conn.send({
+      type: 'system',
+      data: data
+    })
+  }
+  
+  function sendMessage ($store, text) {
+    var message = {
+      from: $store.me,
+      text: text,
+      timestamp: Date.now(),
+    }
+    $store.conn.send(message)
+    $store.messages.push(message)
+  }
+  
+  function receiveMessage ($store, message) {
+    $store.messages.push(message)
+  }
+  
+  function hostPrivateConn ($store, tempPeer, tempConn) {
+    // create new peer connection
+    var peer = new Peer()
+    peer.on('open', function () {
+      $store.status = 'Waiting for handshake on private line'
+      // share new connection id on old connection
+      var message = {id: peer.id}
+      sendSystemMessage(tempConn, message)
+    })
+    
+    peer.on('connection', function(conn) {
+      $store.status = 'Connected to private line'
+      $store.conn = conn
+      sendMessage($store, 'joined')
+      
+      // handle messages received from peer
+      conn.on('data', function(data) {
+        receiveMessage($store, data)
+      })
+      
+      // free-up old connection
+      tempPeer.destroy()
+    })
+  }
+  
+  function hostCommonRoom ($store) {
+    $store.status = 'Hosting room...'
+    var roomID = 'host-common-room'
+    var peer = new Peer(roomID)
+    
+    peer.on('open', function () {
+      $store.status = 'Waiting for someone to join...'
+    })
+    peer.on('error', function (err) {
+      if (err.type === 'unavailable-id') {
+        $store.status = 'Already hosted. Joining them.'
+        return joinCommonRoom($store, roomID)
+      }
+    })
+    
+    peer.on('connection', function(conn) {
+      $store.status = 'Found someone. Creating a new private line.'
+      hostPrivateConn($store, peer, conn)
+    })
+  }
+  
+  function joinPrivateLine ($store, peer, privateID) {
+    $store.status = 'Joining private line...'
+    var conn = peer.connect(privateID)
+    
+    conn.on('open', function() {
+      $store.status = 'Private line established.'
+      $store.conn = conn
+      sendMessage($store, 'joined')
+    })
+    
+    // handle messages received from peer
+    conn.on('data', function(data) {
+      receiveMessage($store, data)
+    })
+  }
+  
+  function joinCommonRoom ($store, roomID) {
+    $store.status = 'Creating connection to system...'
+    var peer = new Peer()
+    peer.on('open', function () {
+      $store.status = 'Joining common room...'
+      var conn = peer.connect(roomID)
+      
+      // on open will be launch when you successfully connect to PeerServer
+      conn.on('open', function() {
+        $store.status = 'Found someone. Waiting for private line...'
+      })
+      
+      // handle messages received from peer
+      conn.on('data', function(data) {
+        joinPrivateLine($store, peer, data.data.id)
+      })
+    })
+  }
+  
+  function findRoom ($store) {
+    hostCommonRoom($store)
+  }
+  
+  function setupEverything () {
+    console.log('setupEverything')
+    document.addEventListener('alpine:init', setupAlpine)
+    window.ChatActions = {
+      sendMessage: sendMessage,
+      findRoom: findRoom,
+    }
+  }
+  setupEverything()
 })()
